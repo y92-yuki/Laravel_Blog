c02104da1284c9343c34ae94ee7d155ec115ab46
y92-yuki <yukiyamanaka92@gmail.com>
2022-05-26 22:56:43 +0900

    GETリクエストをcURL -> Guzzleへ変更

--- app/Services/Forecast.php
+++ app/Services/Forecast.php
@@ -3,15 +3,20 @@
 namespace App\Services;
 
 use Exception;
+use GuzzleHttp\Client;
 
 class Forecast {
+    protected $apiKey;
+    protected $client;
     protected $lat;
     protected $long;
-    protected $apiKey;
 
     
     public function __construct($prefOffice) {
         $this->apiKey = config('forecast.apikey');
+        $this->client = new Client([
+            'base_uri' => config('forecast.baseurl')
+        ]);
         $res = $this->getGeocoding($prefOffice);
         $this->lat = $res['lat'];
         $this->long = $res['long'];
@@ -20,27 +25,15 @@ class Forecast {
 
     protected function getGeocoding($location) {
         //地域名から緯度・経度を取得するためのAPI
-        $locationURL = config('forecast.baseurl') . "geo/1.0/direct?q={$location},JP&appid={$this->apiKey}";
+        $locationURL = "geo/1.0/direct?q={$location},JP&appid={$this->apiKey}";
 
         try {
-            //cURLセッションの初期化
-            $ch = curl_init();
+            //URLとmethodの指定
+            $response = $this->client->get($locationURL);
 
-            //URLとオプションの指定
-            $options = [
-                CURLOPT_URL => $locationURL,
-                CURLOPT_RETURNTRANSFER => true
-            ];
-
-            curl_setopt_array($ch,$options);
-
-            //URLの情報を取得(JSON)
-            $locationJson = curl_exec($ch);
-            curl_close($ch);
-
-            //取得したURLの情報を配列へ変換
-            $location = json_decode($locationJson,true);
-
+            //URLの情報を取得して配列へ変換
+            $location = json_decode($response->getBody(),true);
+            
             return [
                 'lat' => $location[0]['lat'],
                 'long' => $location[0]['lon'],
@@ -52,23 +45,12 @@ class Forecast {
 
     public function getForecast() {
         //緯度・経度情報から天気予報を取得
-        $foreCastURL = config('forecast.baseurl') . "data/2.5/weather?lat={$this->lat}&lon={$this->long}&appid={$this->apiKey}&lang=ja&units=metric";
+        $foreCastURL = "data/2.5/weather?lat={$this->lat}&lon={$this->long}&appid={$this->apiKey}&lang=ja&units=metric";
 
         try {
-            $ch = curl_init();
-
-            $options = [
-                CURLOPT_URL => $foreCastURL,
-                CURLOPT_RETURNTRANSFER => true
-            ];
-
-            curl_setopt_array($ch,$options);
-
-            $foreCast = curl_exec($ch);
-
-            curl_close($ch);
-
-            return json_decode($foreCast,true);
+            $response = $this->client->get($foreCastURL);
+            
+            return json_decode($response->getBody(),true);
         } catch(Exception $e) {
             return json_encode($e->getMessage());
         }
